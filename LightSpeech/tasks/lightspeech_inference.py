2ac46e8250206164235e664bce6636bc322f8eff
Renqian Luo <renqianluo@gmail.com>
2022-04-11 08:06:04 +0000

    add feature of user input

--- LightSpeech/tasks/lightspeech_inference.py
+++ LightSpeech/tasks/lightspeech_inference.py
@@ -2,12 +2,13 @@
 # Licensed under the MIT License.
 
 import os, glob, re
+from tqdm import tqdm
+import numpy as np
+import torch
+import utils
 from utils.hparams import hparams, set_hparams
 from tasks.lightspeech import LightSpeechDataset, LightSpeechTask
-import torch
-import numpy as np
-from tqdm import tqdm
-torch.backends.cudnn.enabled = False
+
 set_hparams()
 
 def get_latest_ckpt(dir):
@@ -43,10 +44,8 @@ with open(hparams['inference_text'], 'r') as f:
     user_text = f.readlines()
 
 # create sample dir inside work_dir in hparams
-gen_dir = os.path.join(hparams['work_dir'], f'inference_{task.global_step}')
+gen_dir = os.path.join(hparams['work_dir'], f"inference_{hparams['inference_text']}_{task.global_step}")
 os.makedirs(gen_dir, exist_ok=True)
-#os.makedirs(f'{gen_dir}/text', exist_ok=True)
-#os.makedirs(f'{gen_dir}/spec', exist_ok=True)
 os.makedirs(f'{gen_dir}/wavs', exist_ok=True)
 os.makedirs(f'{gen_dir}/spec_plot', exist_ok=True)
 os.makedirs(f'{gen_dir}/pitch_plot', exist_ok=True)
@@ -54,13 +53,20 @@ os.makedirs(f'{gen_dir}/pitch_plot', exist_ok=True)
 # perform text-to-speech then save mel and wav
 with torch.no_grad():
     for i, text in enumerate(tqdm(user_text)):
+        text = text.strip()
         phone = torch.LongTensor(dataset.text_to_phone(text))
         phone = phone.unsqueeze(0).cuda() if torch.cuda.is_available() else phone.unsqueeze(0)
-        outputs = task.model(phone, None, None, None, None, None)
-        mel_out = outputs['mel_out'].permute(0, 2, 1) # [1, num_mels, T]
-        noise_outputs = task.remove_padding(outputs.get("noise_outputs"))
-        pitch_pred = task.remove_padding(outputs.get("pitch_pred"))
+        output = task.model(phone, None, None, None, None, None)
+        output['outputs'] = output['mel_out']
+        _output = utils.unpack_dict_to_list(output)[0]
+        output = {}
+        for k, v in _output.items():
+            if type(v) is torch.Tensor:
+                output[k] = v.cpu().numpy()
+        mel_out = task.remove_padding(output['mel_out'])
+        noise_outputs = task.remove_padding(output.get("noise_outputs"))
+        pitch_pred = task.remove_padding(output.get("pitch"))
         wav_out = task.inv_spec(mel_out, pitch_pred, noise_outputs)
         # save mel and wav
-        task.save_result(wav_out, mel_out.cpu()[0].T, f'P', i, text, gen_dir)
+        task.save_result(wav_out, mel_out, f'P', i, text, gen_dir, pitch=pitch_pred)
 
